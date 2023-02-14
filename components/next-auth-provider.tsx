5ccc7f664b7fd816cf8c9b032a277950162abc5c
wobble-house <ross@wobble.house>
2023-02-14 14:49:57 -0500

    tdfgjfsgj

--- components/next-auth-provider.tsx
+++ components/next-auth-provider.tsx
@@ -18,7 +18,7 @@ function Auth({ children }) {
   const { status } = useSession({ required: true })
 
   if (status === "loading") {
-    return <div>Loading...</div>
+    return <div>Loading..</div>
   }
 
   return children
