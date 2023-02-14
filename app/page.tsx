5d7120db902611ed5e61809b30a2f458cfdafcc8
wobble-house <ross@wobble.house>
2023-02-14 15:05:41 -0500

    trying new env variables

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default async function Home() {
     <MyNavbar><LoginButton/></MyNavbar>
     <MyModal><ModalInfo/></MyModal>
     <div className="bggradient2 relative">
-      <title>A2IM Knowledge Base</title>
+      <title>A2IM Knowledge Base!!!</title>
       <meta
           name="description"
           content="Founded by independents, for independents, A2IM Indie Week is a four-day conference for the independent music community in the United States."
